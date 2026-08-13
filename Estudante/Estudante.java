e07486e56e1064b156c8db5b97146b2b7273f750
nicolas-nunes24 <2024301424@aluno.canoas.ifrs.edu.br>
2026-08-13 17:48:47 -0300

    alteracoes em estudante

--- Estudante/Estudante.java
+++ Estudante/Estudante.java
@@ -11,26 +11,52 @@ public Estudante(String nome,Double[] notas){
   this.nome = nome;
   this.notas = notas;
 }
- //getters e setters
-    public String getNome() {
-        return nome;
+ //metodos
+
+    public void lernotas(){
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a nota do estudante: " + this.nome);
+
+    for(int i=0; i<5;i++){
+        System.out.println("Insira a nota " + (i+1) + ": ");
+        this.notas[i] = scanner.nextDouble();// Lê o número e guarda na posição 'i' do array
+    }
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public double calcularmedia(double media){
+
+        double soma= 0.0;
+
+        for(int i=0; i<5;i++){
+            soma += this.notas[i];
+        }
+
+    media= soma / 5;
+
+    return media;
+    }
+
+    public String getNome() {
+        return nome;
     }
 
     public Double[] getNotas() {
         return notas;
     }
 
-    public void setNotas(Double[] notas) {
-        this.notas = notas;
+    public double menornota(){
+
+    double menor=this.notas[0];
+
+        for (int i = 0; i < 5; i++) {
+            if(this.notas[i]<menor) {
+                menor = this.notas[i];
+            }
+        }
+
+    return menor;
     }
 
-    public Double lernotas(Double[] notas){
-    Scanner scanner = new Scanner(System.in);
+    
 
-    return 0.0;
-    }
 }
